26d313505fc3ca2e39eb412e7dafa75d8e90f0b4
Abdul Rohman <33149221+abdul-rohman-lab@users.noreply.github.com>
2024-01-01 01:22:13 +0000

    Finalisasi

--- Dashboard/app.py
+++ Dashboard/app.py
@@ -1,10 +1,21 @@
 
+import streamlit as st
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import streamlit as st
-from babel.numbers import format_currency
+import geopandas
+import folium
 
+from folium.features import CustomIcon
+from folium.plugins import MarkerCluster
+from folium import Circle, Marker
+from folium import GeoJson
+from streamlit_folium import st_folium
+
+import requests
+import branca
+from babel.numbers import format_currency
 
 
 
@@ -48,9 +59,11 @@ def create_rfm_df(df):
 
 
 # Load cleaned data
-all_df = pd.read_csv("https://raw.githubusercontent.com/abdul987a/resource/main/all_data.csv")
+all_df = pd.read_csv("https://raw.githubusercontent.com/abdul-rohman-lab/projek_olist/main/Dashboard/otp_orders_customers.csv")
 poi_df = pd.read_csv("https://raw.githubusercontent.com/abdul987a/resource/main/poi_data.csv")
+revenue_from_customer_geo = pd.read_csv("https://raw.githubusercontent.com/abdul-rohman-lab/projek_olist/main/Dashboard/revenue_from_customer_geo.csv")
 
+st.set_page_config(layout="wide")
 
 #st.dataframe(data=all_df)
 
@@ -67,6 +80,9 @@ max_date = all_df["order_purchase_timestamp"].max()
 
 
 with st.sidebar:
+    # Menambahkan logo perusahaan
+    st.image("https://github.com/abdul-rohman-lab/projek_olist/blob/main/logo_olist_d7309b5f20.png")
+
     # Menambahkan logo perusahaan
     st.subheader("Olist Store")
 
@@ -90,7 +106,7 @@ rfm_df = create_rfm_df(main_df)
 
 # plot number of daily orders (2021)
 st.header('Olist Store Dashboard :sparkles:')
-st.subheader('Daily - Orders')
+st.subheader('Daily Orders')
 
 col1, col2 = st.columns(2)
 
@@ -103,6 +119,9 @@ with col2:
     st.metric("Total Revenue", value=total_revenue)
 
 
+# ===========================================
+st.subheader("Tren Order Olist Shop")
+
 fig, ax = plt.subplots(figsize=(30, 10))
 ax.plot(
     daily_orders_df["order_purchase_timestamp"],
@@ -113,6 +132,7 @@ ax.plot(
 )
 ax.tick_params(axis='y', labelsize=20)
 ax.tick_params(axis='x', labelsize=15)
+ax.set_title('Tren Order 2016=2018', loc="center", fontsize=20, pad=20)
 for x,y in zip(daily_orders_df["order_purchase_timestamp"], daily_orders_df["order_count"]):
 
     label = "{:.2f}".format(y)
@@ -125,22 +145,23 @@ for x,y in zip(daily_orders_df["order_purchase_timestamp"], daily_orders_df["ord
 
 st.pyplot(fig)
 
-
+#===========================================================
 # Product performance
-st.subheader("Best & Worst Performing Product")
+st.subheader("Best & Worst Performing Category Product")
 
 #st.dataframe(sum_order_items_df)
 
-fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(24, 6))
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(24, 12))
 
 colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
-sns.barplot(x="quantity_x", y="category", data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
+sns.barplot(x="quantity_x", y="category", data=sum_order_items_df.sort_values('quantity_x', ascending=False).head(5), palette=colors, ax=ax[0])
 ax[0].set_ylabel(None)
 ax[0].set_xlabel("Number of Sales", fontsize=30)
-ax[0].set_title("Best Performing Product", loc="center", fontsize=50)
+ax[0].set_title("Best Performing Product", loc="center", fontsize=50, pad=20)
 ax[0].tick_params(axis='y', labelsize=35)
 ax[0].tick_params(axis='x', labelsize=30)
+ax[0].bar_label(ax[0].containers[0], label_type='center', fontsize=20)
 
 sns.barplot(x="quantity_x", y="category", data=sum_order_items_df.sort_values(by="quantity_x", ascending=True).head(5), palette=colors, ax=ax[1])
 ax[1].set_ylabel(None)
@@ -148,13 +169,166 @@ ax[1].set_xlabel("Number of Sales", fontsize=30)
 ax[1].invert_xaxis()
 ax[1].yaxis.set_label_position("right")
 ax[1].yaxis.tick_right()
-ax[1].set_title("Worst Performing Product", loc="center", fontsize=50)
+ax[1].set_title("Worst Performing Product", loc="center", fontsize=50, pad=20)
 ax[1].tick_params(axis='y', labelsize=35)
 ax[1].tick_params(axis='x', labelsize=30)
+ax[1].bar_label(ax[1].containers[0], label_type='center', fontsize=20)
 
 st.pyplot(fig)
 
+# ===============================================================
 
+st.subheader("5 Customer Cities with the highest and lowest revenue")
+
+city_revenue = all_df.groupby('customer_city', as_index=False).agg({'total_price': 'sum'})
+city_revenue['%_revenue'] = (city_revenue.total_price/city_revenue.total_price.sum())*100
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(24, 10))
+
+colors = ["#72BCD4", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+
+sns.barplot(city_revenue.sort_values(by="%_revenue", ascending=False).head(5), x="customer_city", y="%_revenue", palette=colors, ax=ax[0])
+ax[0].set_ylabel('Persen Revenue', fontsize=12)
+ax[0].set_xlabel(None)
+# ticks = [0, 500000, 1500000, 2000000]
+# ax[0].set_yticks(ticks)
+ax[0].set_title("Pendapatan 5 terbesar per Kota Customer", loc="center", fontsize=15)
+ax[0].tick_params(axis ='y', labelsize=12)
+ax[0].tick_params(axis ='x', labelsize=20, rotation=90)
+ax[0].bar_label(ax[0].containers[0], label_type='center', fontsize=12)
+
+
+
+sns.barplot(city_revenue.sort_values(by="%_revenue").head(5), x="customer_city", y="%_revenue", palette=colors, ax=ax[1])
+ax[1].set_ylabel('Persen Revenue', fontsize=12)
+ax[1].set_xlabel(None)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Pendapatan 5 terkecil per kota Customer", loc="center", fontsize=15)
+ax[1].tick_params(axis='y', labelsize=12)
+ax[1].tick_params(axis ='x', labelsize=20, rotation=90)
+ax[1].bar_label(ax[1].containers[0], label_type='center', fontsize=12)
+
+plt.suptitle("5 Kota Customer Dengan Revenue Yang Terbanyak dan Terkecil", fontsize=20)
+plt.show()
+st.pyplot(fig)
+
+
+# ================================================================
+st.subheader("Best & Worst Revenue From Customer")
+
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(24, 10))
+
+colors = ["#72BCD4", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
+
+sns.barplot(all_df.sort_values(by="total_price", ascending=False).head(5), x="customer_unique_id", y="total_price", palette=colors, ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel(None)
+ax[0].set_title("Pendapatan 5 Customer terbesar", loc="center", fontsize=15)
+ax[0].tick_params(axis ='y', labelsize=12)
+ax[0].tick_params(axis ='x', labelsize=12, rotation=90)
+ax[0].bar_label(ax[0].containers[0], label_type='center', fontsize=12)
+
+
+
+sns.barplot(all_df.sort_values(by="total_price").head(5), x="customer_unique_id", y="total_price", palette=colors, ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel(None)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].set_title("Pendapatan 5 Customer Terkecil", loc="center", fontsize=15)
+ax[1].tick_params(axis='y', labelsize=12)
+ax[1].tick_params(axis ='x', labelsize=12, rotation=90)
+ax[1].bar_label(ax[1].containers[0], label_type='center', fontsize=12)
+
+plt.suptitle("5 Pendapatan Terbesar & Terkecil Dari Customer ID", fontsize=20)
+plt.show()
+st.pyplot(fig)
+
+
+#=================================================================
+
+st.subheader("Geoanalysist Sebaran Total Revenue Per State")
+
+revenue_perstate = all_df.groupby(by='customer_state').agg({'total_price': 'sum'})
+revenue_perstate['in_%'] = (revenue_perstate.total_price/revenue_perstate.total_price.sum())*100
+
+
+state_geo = requests.get(
+    "https://raw.githubusercontent.com/giuliano-oliveira/geodata-br-states/main/geojson/br_states.json"
+).json()
+
+states = geopandas.GeoDataFrame.from_features(state_geo, crs="EPSG:4326")
+
+df = pd.merge(
+    left = states,
+    right = revenue_perstate,
+    how = 'left',
+    left_on = 'SIGLA',
+    right_on = 'customer_state',
+)
+
+colormap = branca.colormap.LinearColormap(
+    vmin=df["in_%"].quantile(0.0),
+    vmax=df["in_%"].quantile(1),
+    colors=["red", "orange", "lightblue", "green", "darkgreen"],
+    caption="State Level Median County Household Income (%)",
+)
+
+m = folium.Map(location=[-7.656919199019851, -55.3408624450622], zoom_start=4)
+
+popup = folium.GeoJsonPopup(
+    fields=["SIGLA", "in_%"],
+    aliases=["State", "% Revenue"],
+    localize=True,
+    labels=True,
+    style="background-color: yellow;",
+)
+
+tooltip = folium.GeoJsonTooltip(
+    fields=["SIGLA", "in_%"],
+    aliases=["State:", "% Revenue"],
+    localize=True,
+    sticky=False,
+    labels=True,
+    style="""
+        background-color: #F0EFEF;
+        border: 2px solid black;
+        border-radius: 3px;
+        box-shadow: 3px;
+    """,
+    max_width=800,
+)
+
+
+g = folium.GeoJson(
+    df,
+    style_function=lambda x: {
+        "fillColor": colormap(x["properties"]["in_%"])
+        if x["properties"]["in_%"] is not None
+        else "transparent",
+        "color": "black",
+        "fillOpacity": 0.4,
+    },
+    tooltip=tooltip,
+    popup=popup,
+).add_to(m)
+
+colormap.add_to(m)
+
+
+#st_data = st_folium(m, width=725)
+st_data = st_folium(m)
+
+
+
+
+
+
+
+#==================================================================
 # Best Customer Based on RFM Parameters
 st.subheader("Best Customer Based on RFM Parameters")
 
@@ -201,5 +375,31 @@ ax[2].tick_params(axis='x', labelsize=20, rotation = 90)
 ax[2].bar_label(ax[2].containers[0], label_type='center')
 
 st.pyplot(fig)
+
+#=====================================================
+# Cluster Segment Customers
+st.subheader("Cluster Segment Customers")
+
+rfm_df['segment'] = 'low value'
+rfm_df.loc[rfm_df['frequency'] > 3, 'segment'] = 'mid_value'
+rfm_df.loc[rfm_df['frequency'] > 6, 'segment'] = 'high_value'
+
+segment = rfm_df.groupby(by='segment', as_index=False).agg({'customer_unique_id': 'nunique'})
+segment['in_%'] = (segment.customer_unique_id/segment.customer_unique_id.sum())*100
+
+
+fig = plt.figure(figsize=(10, 4))
+ax = sns.barplot(x='segment', y='in_%',
+                 data=segment,
+                 errwidth=0)
+
+for i in ax.containers:
+    ax.bar_label(i,)
+
+st.pyplot(fig)
+
+
+for i in ax.containers:
+    ax.bar_label(i,)
 
 st.caption('Copyright © Abdul Rohman 2023')
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-## Untuk Notebook saya menggunakan Google Colab
+# Olist Shop Dashboard ✨
+
+## Setup environment
+```
+conda create --name main-ds python=3.9
+conda activate main-ds
+pip install numpy pandas scipy matplotlib seaborn jupyter streamlit babel geopandas folium streamlit-folium requests branca
+```
+
+## Run steamlit app
+```
+streamlit run app.py
+```
